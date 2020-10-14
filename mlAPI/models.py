1c4efb5fad14f3657c81fc38dc1bac8be8ef519f
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-10-14 23:01:16 +0300

    http requests

--- mlAPI/models.py
+++ mlAPI/models.py
@@ -2,12 +2,19 @@ from django.db import models
 
 # Create your models here.
 class FoodRemainingTimes(models.Model):
-    Food_Amount_gr = models.IntegerField()
 
-    Location_Id_1 = models.IntegerField()
-    Location_Id_2 = models.IntegerField()
-    Location_Id_3 = models.IntegerField()
-    Location_Id_4 = models.IntegerField()
+    Food_Amount_gr = models.FloatField(null=True, blank=True)
+    Location_Id = models.IntegerField(null=True, blank=True)
+    Longitude = models.FloatField(null=True, blank=True)
+    Latitude = models.FloatField(null=True, blank=True)
+    Prediction = models.FloatField(null=True, blank=True)
 
-    def __int__(self):
-        return self.Food_Amount_gr
+    def serialize(self):
+        return {
+            "id": self.id,
+            "Food_Amount_gr": self.Food_Amount_gr,
+            "Location_Id": self.Location_Id,
+            "Longitude": self.Longitude,
+            "Latitude": self.Latitude,
+            "Prediction": self.Prediction
+        }
--- mlAPI/views.py
+++ mlAPI/views.py
@@ -5,8 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-from . serializers import FoodRemainingTimesSerializers
-from . models import FoodRemainingTimes
+from .serializers import FoodRemainingTimesSerializers
+from .models import FoodRemainingTimes
 from django.conf import settings
 
 # Create your views here.
@@ -16,17 +16,57 @@ import json
 import pickle
 import os
 
+
 class FoodRemainingTimesView(viewsets.ModelViewSet):
     queryset = FoodRemainingTimes.objects.all()
     serializer_class = FoodRemainingTimesSerializers
 
-@api_view(["POST"])
+
+@api_view(["POST", "GET"])
 def PredictRemainingTime(request):
-    try:
-        model = pickle.load(open(os.path.join(settings.MODELS, "random_forest.pkl"), 'rb'))
-        data = request.data
-        values = np.array(list(data.values())).reshape(1, -1)
-        prediction = model.predict(values)
-        return JsonResponse("Remaining Time is {}".format(prediction), safe=False)
-    except ValueError as e:
-        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+
+    # POST METHOD
+    if request.method == "POST":
+        number_of_locations = 5
+        try:
+
+            # Upload the model
+            model = pickle.load(open(os.path.join(settings.MODELS, "random_forest.pkl"), 'rb'))
+
+            # Get the data
+            data = request.data
+
+            # Create column names
+            dictionary = {"Food_Amount_gr": [data["Food_Amount_gr"]]}
+            for i in range(1, number_of_locations):
+                if i != data["Location_Id"]:
+                    dictionary[str(i)] = [0]
+                else:
+                    dictionary[str(i)] = [1]
+
+            # Create a DataFrame
+            df = pd.DataFrame.from_dict(dictionary)
+
+            # Make Prediction
+            prediction = model.predict(df.values)
+
+            # Add prediction to the data
+            data.update({"Prediction":prediction})
+
+            serializer = FoodRemainingTimesSerializers(data=data)
+            if serializer.is_valid():
+                serializer.save()
+            return Response(status.HTTP_200_OK)
+        except ValueError as e:
+            return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+
+    # GET METHOD
+    elif request.method == "GET":
+
+        # Get all remaining times
+        data = FoodRemainingTimes.objects.all()
+
+        # Serialize
+        serializer = FoodRemainingTimesSerializers(data, many=True)
+
+        return Response(serializer.data)
