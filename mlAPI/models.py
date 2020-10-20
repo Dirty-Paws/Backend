8f66df9cbab385195ebb37650965dffb70ce0e26
Barış Hasdemir <33374731+Bhasfe@users.noreply.github.com>
2020-10-20 14:56:41 +0300

    foor or not

--- mlAPI/models.py
+++ mlAPI/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class FoodRemainingTimes(models.Model):
     Food_Amount_gr = models.FloatField(null=True, blank=True)
     Location_Id = models.IntegerField(null=True, blank=True)
@@ -11,6 +10,7 @@ class FoodRemainingTimes(models.Model):
 
     def __str__(self):
         return f"{self.Food_Amount_gr} {self.Location_Id} {self.Longitude} {self.Latitude} {self.Location_Id} {self.Prediction}"
+
 
 class Emergency(models.Model):
     message = models.CharField(max_length=200, null=True, blank=True)
@@ -25,8 +25,8 @@ class Emergency(models.Model):
     def __str__(self):
         return f"{self.message} {self.owner_id} {self.user_id} {self.status} {self.image} {self.Location_Id} {self.Longitude} {self.Latitude}"
 
-class FoodStatus(models.Model):
 
+class FoodStatus(models.Model):
     IsFoodFinished = models.IntegerField(null=True, blank=True)
     Location_Id = models.IntegerField(null=True, blank=True)
     Longitude = models.FloatField(null=True, blank=True)
@@ -34,3 +34,15 @@ class FoodStatus(models.Model):
 
     def __str__(self):
         return f"{self.IsFoodFinished} {self.Location_Id} {self.Longitude} {self.Latitude}"
+
+
+class FoodOrNot(models.Model):
+    ImageBowl = models.CharField(max_length=1000, null=True, blank=True)
+    IsFood = models.IntegerField(null=True, blank=True)
+    Resolved = models.IntegerField(null=True, blank=True)
+    Location_Id = models.IntegerField(null=True, blank=True)
+    Longitude = models.FloatField(null=True, blank=True)
+    Latitude = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ImageBowl} {self.IsFood} {self.Resolved} {self.Location_Id} {self.Longitude} {self.Latitude}"
